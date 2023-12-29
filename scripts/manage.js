90ed50718375ee04fe1d335fb0f679b18ccfa31c
HRay <hray3182@gmail.com>
2023-12-29 21:27:09 +0800

    Add edit functionality to manage.html and manage.js

--- scripts/manage.js
+++ scripts/manage.js
@@ -1,29 +1,54 @@
 import { menuData } from "./menu.js";
 
-console.log(menuData)
 
 function createItems(data) {
-//     <tr data-key="1">
-//     <td class="img"><img src="./assets/shushi_img/1b6b817b-85b9-4303-b29d-4a01f6e6a65b.jpg"></td>
-//     <td class="name">鰻魚握壽司</td>
-//     <td class="catregory">握壽司</td>
-//     <td class="price">100</td>
-// </tr>
     const items = [];
 
     for (let i = 0; i < data.length; i++) {
         const item = document.createElement("tr");
-        item.setAttribute("data-key", data[i].id);
+        item.setAttribute("data-key", data[i].key);
         item.innerHTML = `
             <td class="img"><img src="./assets/shushi_img/${data[i].img_file}"></td>
             <td class="name">${data[i].name}</td>
             <td class="catregory">${data[i].category}</td>
             <td class="price">${data[i].price}</td>
+            <td class="operation">
+                <a class="icon edit"><img  src="./assets/edit.svg" alt=""></a>
+                <a class="icon delete"><img  src="./assets/delete.svg" alt=""></a>
+            </td>
         `;
         items.push(item);
     }
     // return html 
     return items;
+}
+
+function handleDelete() {
+    console.log("delete")
+}
+
+function handleEdit() {
+    console.log("edit")
+}
+
+function handleAdd() {
+
+}
+
+function deleleteListener() {
+    // a.icon.delete
+    const deleteButtons = document.querySelectorAll("a.icon.delete");
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", handleDelete);
+    })
+}
+
+function editListener() {
+    // a.icon.edit
+    const editButtons = document.querySelectorAll("a.icon.edit");
+    editButtons.forEach(button => {
+        button.addEventListener("click", handleEdit);
+    })
 }
 
 // 當文檔加載完成後，將菜單添加到 .table 元素中
@@ -32,7 +57,14 @@ document.addEventListener('DOMContentLoaded', () => {
     if (target) {
         const menu = createItems(menuData);
         target.append(...menu);
+
+        deleleteListener()
+        editListener()
+
     } else {
-        console.error("未找到類別為 'content' 的 div 元素");
+        console.error("未找到類別為 'table' 的 div 元素");
     }
 });
+
+
+

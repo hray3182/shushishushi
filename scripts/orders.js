ef9b6537241c8ca9c2d23603dc19d42bd4f559ac
HRay <hray3182@gmail.com>
2023-12-30 19:31:14 +0800

    Add order page

---
+++ scripts/orders.js
@@ -0,0 +1,71 @@
+import { menuData } from "./menu.js";
+
+function generateRandomOrder() {
+    const hour = Math.floor(Math.random() * 12) + 1;
+    const timezone = (hour > 6 && hour < 12) ? "上午" : "下午";
+    const minute = Math.floor(Math.random() * 60);
+    const time = `${timezone} ${hour}:${minute}`;
+
+    const tableNum = Math.floor(Math.random() * 10) + 1;
+    const table = `${tableNum}桌`;
+
+    const itemsNum = Math.floor(Math.random() * 5) + 1;
+    const items = [];
+    for (let i = 0; i < itemsNum; i++) {
+        const item = menuData[Math.floor(Math.random() * menuData.length)];
+        items.push(item);
+    }
+    
+    let total = 0
+
+    const trs = items.map(item => {
+        const quantity = Math.floor(Math.random() * 5) + 1;
+        const subtotal = item.price * quantity
+        total += subtotal
+        return `
+            <tr>
+                <td class="name">${item.name}</td>
+                <td class="quantity">x${quantity}</td>
+                <td class="subtotal">$${subtotal}</td>
+            </tr>
+
+        `
+    }).join("")
+
+    const html = `
+    <div class="order">
+    <div class="order-head">
+        <a id="time">${time}</a>
+        <a id="table">${table}</a>
+    </div>
+    <div class="order-body">
+        <div class="items">
+            <table>
+                ${trs}
+            </table>
+        </div>
+    </div>
+    <div class="total">
+    合計: ${total}元
+</div>
+</div>`
+
+    return html;
+}
+
+function createOrder(num) {
+    const orders = [];
+    for (let i = 0; i < num; i++) {
+        const order = generateRandomOrder();
+        orders.push(order);
+    }
+    return orders;
+}
+
+function init() {
+    const orders = createOrder(10);
+    const body = document.querySelector(".orders");
+    body.innerHTML = orders.join("");
+}
+
+init()
